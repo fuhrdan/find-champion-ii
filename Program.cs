900095a8c22cf65ca472403269c4f4709dcd4615
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-26 06:13:59 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//*****************************************************************************
+//** 2924. Find Champion II      leetcode                                    **
+//*****************************************************************************
+
+int findChampion(int n, int** edges, int edgesSize, int* edgesColSize) {
+    int indeg[n];
+    memset(indeg, 0, sizeof(indeg));
+
+    for(int i = 0; i < edgesSize; i++)
+    {
+        indeg[edges[i][1]]++;
+    }
+
+    int ans = -1;
+    int count = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if (indeg[i] == 0)
+        {
+            count++;
+            ans = i;
+        }
+    }
+
+    return (count == 1) ? ans : -1;
+}
